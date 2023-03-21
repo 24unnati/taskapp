56ee2297657a2b00fdaee9cb6bf0025ae7e8e402
24unnati <120442728+24unnati@users.noreply.github.com>
2023-03-22 00:32:36 +0530

    Add file for lodash and moment functins

--- src/components/Lodash.js
+++ src/components/Lodash.js
@@ -3,17 +3,60 @@ import _ from 'lodash';
 
 export const Lodash = () => {
   const max = _.max([10, 8, 25, 45, 2, 5, 8, 9]);
-  const sum = _.sum([10, 8, 25, 45, 2, 5, 8, 9]);
-  const filter = _.filter([10, 8, 25, 45, 2, 5, 8, 9]);
-  const isEmpty = _.isEmpty([10, 8, 25, 45, 2, 5, 8, 9]);
+  console.log('max', max); // 45
 
+  const sum = _.sum([10, 8, 25, 45, 2, 5, 8, 9]);
+  console.log('sum', sum); //112
+
+  const filter = _.filter([10, 8, 25, 45, 2, 5, 8, 9]);
+  console.log(('filter', filter)); //[10, 8, 25, 45, 2, 5, 8, 9]
+
+  const isEmpty = _.isEmpty([10, 8, 25, 45, 2, 5, 8, 9]);
+  console.log(('isEmpty', isEmpty)); //false
+
+  const students = [
+    { name: 'Alice', grade: 90 },
+    { name: 'Bob', grade: 80 },
+  ];
+  const uniqueGrades = _.uniqBy(students, (student) => student.grade);
+  console.log('uniqueGrades', uniqueGrades); //[{ name: 'Alice', grade: 90 }, { name: 'Bob', grade: 80 }]
+
+  const animals = [
+    { name: 'Dog', type: 'Mammal' },
+    { name: 'Cat', type: 'Mammal' },
+    { name: 'Bird', type: 'Bird' },
+    { name: 'Fish', type: 'Fish' },
+  ];
+  const groupedAnimals = _.groupBy(animals, (animal) => animal.type);
+  console.log('groupedAnimal', groupedAnimals); // { Mammal: [ { name: 'Dog', type: 'Mammal' }, { name: 'Cat', type: 'Mammal' } ], Bird: [ { name: 'Bird', type: 'Bird' } ], Fish: [ { name: 'Fish', type: 'Fish' } ] }
+
+  const numbers = [1, 2, 3, 4, 5];
+  const doubledNumbers = _.map(numbers, (n) => n * 2);
+  console.log('doubledNumbers', doubledNumbers); // [2, 4, 6, 8, 10]
+
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+  const chunks = _.chunk(letters, 2);
+  console.log('chunks', chunks); // [['a', 'b'], ['c', 'd'], ['e', 'f']]
+
+  const isTrue = true;
+  const isFalse = false;
+  console.log(_.isBoolean(isTrue)); // true
+  console.log(_.isBoolean(isFalse)); // true
+  console.log(_.isBoolean('not a boolean')); // false
   return (
     <div>
-      <h1>Lodash </h1>
-      <h3>{max}</h3>
+      <h1>Lodash.js library function</h1>
+      <h2 style={{ color: 'red', marginTop: '30px' }}>
+        For lodash function open console
+      </h2>
+      {/* <h3>{max}</h3>
       <h3>{sum}</h3>
       <h3>{filter}</h3>
       <h3>{isEmpty}</h3>
+      <h3>{uniqueGrades}</h3>
+      <h3>{groupedAnimals}</h3>
+      <h3>{doubledNumbers}</h3>
+      <h3>{chunks}</h3> */}
     </div>
   );
 };
--- src/components/Moment.js
+++ src/components/Moment.js
@@ -2,12 +2,39 @@ import moment from 'moment';
 import React from 'react';
 
 export const Moment = () => {
-  const mydate = moment().format('YYYY MMMM DD');
-  console.log(mydate);
+  const isoDate = '2023-03-21T12:00:00Z';
+  const unixTimestamp = moment(isoDate).unix();
+  // console.log(unixTimestamp); // Output: 1671648000
+
+  const newUnixTimestamp = 1671648000;
+  const formattedDate = moment.unix(newUnixTimestamp).format('MMMM Do, YYYY');
+  // console.log(formattedDate); // Output: March 21st, 2023
+
+  const originalDate = '03/21/2023';
+  const newFormat = 'DD-MMM-YYYY';
+  const convertedDate = moment(originalDate, 'MM/DD/YYYY').format(newFormat);
+  // console.log(convertedDate); // Output: 21-Mar-2023
+
+  const date1 = moment('2023-03-21');
+  const date2 = moment('2023-04-01');
+  const diffInDays = date2.diff(date1, 'days');
+  // console.log(diffInDays); // Output: 11
+
+  const newOriginalDate = '2023-03-21';
+  const daysToAdd = 7;
+  const newDate = moment(newOriginalDate)
+    .add(daysToAdd, 'days')
+    .format('MMMM Do, YYYY');
+  // console.log(newDate); // Output: March 28th, 2023
+
   return (
     <div>
-      <h1>Moment</h1>
-      <h2 style={{ color: 'blue' }}> {mydate}</h2>
+      <h1 style={{ marginTop: '100px' }}>Moment.js library functions</h1>
+      <h2 style={{ color: 'blue' }}>{unixTimestamp}</h2>
+      <h2 style={{ color: 'blue' }}>{formattedDate}</h2>
+      <h2 style={{ color: 'blue' }}>{convertedDate}</h2>
+      <h2 style={{ color: 'blue' }}>{diffInDays}</h2>
+      <h2 style={{ color: 'blue' }}>{newDate}</h2>
     </div>
   );
 };
