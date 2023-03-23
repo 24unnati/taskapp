69aae867fb522c08d3a2dd08ff15c4f948a0011f
Your Name <you@example.com>
2023-03-23 10:57:28 +0530

    Remove lodash file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Lodash } from "./components/Lodash";
 import { Moment } from "./components/Moment";
 
 function App() {
   return (
     <div className="App">
-      <Lodash />
       <Moment />
     </div>
   );
